4536908fedec135d8cb70da951a72cefe472c13a
marisa-edgar <marisaedgar1212@gmail.com>
2022-04-05 09:04:07 -0700

    prep for migration

--- StarTrek/Models/Character.cs
+++ StarTrek/Models/Character.cs
@@ -5,7 +5,5 @@ namespace StarTrek.Models
     public int CharacterId { get; set; }
     public string Name { get; set; }
     public string Bio { get; set; }
-    public int TrekEventId { get; set; }
-    public virtual TrekEvent TrekEvent { get; set; }
   }
 }
